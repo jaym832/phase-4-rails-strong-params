16dc2befeff35705ca3c04d44b300d6737a21c86
Javier Marin <jjmarin777@gmail.com>
2021-06-09 15:22:21 -0500

    commit tree c2c688e1bd7673e0d0fd380e1d3df43b15af4cfd

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,12 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    ##Strong params, controls what the user has control over 
+
+    # bird = Bird.create(params.permit(:name, :species))
+    
+    ##cleaner code version
+    bird=Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -21,5 +26,12 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 
 end
